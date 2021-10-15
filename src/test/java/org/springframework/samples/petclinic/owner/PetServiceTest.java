847e634258e1b8e56631c051dcc6b8c369dbcc3d
Reza Qavi <rezaqavi1378@gmail.com>
2021-10-15 13:54:37 +0330

    Fix parametrized test

--- src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
@@ -1,6 +1,5 @@
 package org.springframework.samples.petclinic.owner;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -8,50 +7,46 @@ import org.junit.runners.Parameterized.Parameters;
 
 import java.util.Arrays;
 import java.util.Collection;
-import static org.mockito.Mockito.mock;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(Parameterized.class)
 public class PetServiceTest {
 
-	public PetService petService;
+	private final PetService petService;
 
-	Owner owner;
 	static Pet jack, jhon, frank;
-	public int petId;
-	public Pet expectedPet;
+	private final int petId;
+	private final Pet expectedPet;
 
-	@Before
-	public void setup() {
-		petService = mock(PetService.class);
-		jack = new Pet(); jack.setId(11);
-		jhon = new Pet(); jhon.setId(15);
-		frank = new Pet(); frank.setId(15);
-		owner = new Owner();
-		petService.savePet(jack, owner);
-		petService.savePet(jhon, owner);
-		petService.savePet(frank, owner);
-	}
 
 	public PetServiceTest(int petId, Pet expectedPet) {
 		this.petId = petId;
 		this.expectedPet = expectedPet;
+		petService = mock(PetService.class);
 	}
 
 	@Parameters
 	public static Collection<Object[]> parameters() {
+		jack = new Pet(); jack.setId(111);
+		jhon = new Pet(); jhon.setId(121);
+		frank = new Pet(); frank.setId(151);
 		return Arrays.asList(new Object[][] {
-			{11, jack},
-			{12, jhon},
-			{15, frank}}
+			{111, jack},
+			{121, jhon},
+			{151, frank}}
 		);
 	}
 
 	@Test
 	public void testFindPet() {
+		when(petService.findPet(111)).thenReturn(jack);
+		when(petService.findPet(121)).thenReturn(jhon);
+		when(petService.findPet(151)).thenReturn(frank);
 		System.out.println("Test for Id : " + petId);
-		assertEquals(petService.findPet(petId), expectedPet);
+		assertTrue(expectedPet == petService.findPet(petId));
 	}
 
 }

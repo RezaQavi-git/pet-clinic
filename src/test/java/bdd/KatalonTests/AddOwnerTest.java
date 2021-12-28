cecb97686f59a67a5c86ec5874e53985734a4993
Reza Qavi <rezaqavi1378@gmail.com>
2021-12-28 21:02:31 +0330

    Add UI recorded tests

---
+++ src/test/java/bdd/KatalonTests/AddOwnerTest.java
@@ -0,0 +1,89 @@
+package bdd.KatalonTests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class AddOwnerTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+  	System.setProperty("webdriver.chrome.driver", "C:\\Downloads\\chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testAddOwner() throws Exception {
+    driver.get("http://localhost:8080/");
+    driver.findElement(By.linkText("Find owners")).click();
+    driver.findElement(By.linkText("Add Owner")).click();
+    driver.findElement(By.id("firstName")).click();
+    driver.findElement(By.id("firstName")).clear();
+    driver.findElement(By.id("firstName")).sendKeys("Reza");
+    driver.findElement(By.id("lastName")).clear();
+    driver.findElement(By.id("lastName")).sendKeys("Qavi");
+    driver.findElement(By.id("address")).clear();
+    driver.findElement(By.id("address")).sendKeys("Tehran");
+    driver.findElement(By.id("city")).clear();
+    driver.findElement(By.id("city")).sendKeys("Tehran");
+    driver.findElement(By.id("telephone")).clear();
+    driver.findElement(By.id("telephone")).sendKeys("02112365478");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
---
+++ src/test/java/bdd/KatalonTests/AddPetToOwnerTest.java
@@ -0,0 +1,91 @@
+package bdd.KatalonTests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class AddPetToOwnerTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Downloads\\chromedriver.exe");
+	  driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testAddPetToOwner() throws Exception {
+    driver.get("http://localhost:8080/");
+    driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[2]/a/span[2]")).click();
+    driver.findElement(By.id("lastName")).click();
+    driver.findElement(By.id("lastName")).clear();
+    driver.findElement(By.id("lastName")).sendKeys("Qavi");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.xpath("//table[@id='owners']/tbody/tr[2]/td/a")).click();
+    driver.findElement(By.xpath("//a[contains(text(),'Add\n      New Pet')]")).click();
+    driver.findElement(By.id("name")).click();
+    driver.findElement(By.id("name")).clear();
+    driver.findElement(By.id("name")).sendKeys("Topoli");
+    driver.findElement(By.id("birthDate")).click();
+    driver.findElement(By.id("birthDate")).clear();
+    driver.findElement(By.id("birthDate")).sendKeys("2021-01-05");
+    driver.findElement(By.id("type")).click();
+    new Select(driver.findElement(By.id("type"))).selectByVisibleText("cat");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
